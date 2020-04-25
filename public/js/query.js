2c59e9e7b7038efe8659120086f110b64a8d47a4
Toni Hämäläinen <Toni.Hamalainen3@metropolia.fi>
2020-04-26 00:07:32 +0300

    front end testing...

--- public/js/query.js
+++ public/js/query.js
@@ -4,17 +4,48 @@ let data = [];
 let xmlhttp = new XMLHttpRequest();
 let url = 'http://localhost:3000/lol';
 let ul = document.querySelector('ul');
+let main = document.getElementById('main');
 
 xmlhttp.onreadystatechange = () => {
-  if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-      data = JSON.parse(xmlhttp.responseText);
-      //console.log('dataa: ', data);
-      displayResults(data);
-  }
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        data = JSON.parse(xmlhttp.responseText);
+        //console.log('dataa: ', data);
+        Results(data);
+    }
 };
 xmlhttp.open('GET', url, true);
 xmlhttp.send();
 
+const displayResults = (data) => {
+
+        for (let i = 0; i < data.length; i++) {
+            console.log('opponents:', data[i].opponents[0]);
+
+            let divi = document.createElement('div');
+            divi.className = 'w3-row-padding';
+
+            let divi2 = document.createElement('div');
+            divi2.className = 'w3-third w3-container w3-margin-bottom';
+            let divi3 = document.createElement('div');
+            divi3.className = 'w3-container w3-white';
+
+            let team1 = document.createElement('p');
+            let team2 = document.createElement('p');
+            team1.innerText = (data[i].opponents !== 'undefined' && data[i].opponents[0] !== undefined) ? data[i].opponents[0].opponent.name : 'null';
+            team2.innerText = (data[i].opponents !== 'undefined' && data[i].opponents[1] !== undefined) ? data[i].opponents[1].opponent.name : 'null';
+
+            divi3.appendChild(team1);
+            divi3.appendChild(team2);
+
+            divi2.appendChild(divi3);
+
+            divi.appendChild(divi2);
+
+            main.appendChild(divi);
+        }
+};
+
+/*
 const displayResults = (data) => {
     for (const result of data) {
         //console.log('wiineri', result.winner);
@@ -27,4 +58,26 @@ const displayResults = (data) => {
         </li>
         `;
     }
+};
+ */
+
+const Results = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        if (i % 3 === 0) {
+            console.log('diviä');
+            main.innerHTML += `${i > 0 ? '</div>' : ''}
+            <div class="w3-row-padding">
+            `;
+
+        }
+        main.innerHTML += `
+            <div class="w3-third w3-container w3-margin-bottom">
+                <div class="w3-container w3-white">
+                    <p>${(data[i].opponents !== 'undefined' && data[i].opponents[0] !== undefined) ? data[i].opponents[0].opponent.name : 'null'}</p>
+                    <p>${(data[i].opponents !== 'undefined' && data[i].opponents[1] !== undefined) ? data[i].opponents[1].opponent.name : 'null'}</p>
+                </div>
+            </div>
+        `;
+    }
+    main.innerHTML += `</div>`;
 };
